ced8fa74541534fa6150428fb88694191a022f8c
John Wilker <johnwilker2@gmail.com>
2023-02-28 11:32:53 -0300

    options icons added into feedbacks app

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -4,6 +4,8 @@ import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
+import { Options } from '../Options';
+
 import { styles } from './styles';
 import { theme } from '../../theme';
 
@@ -31,7 +33,10 @@ function Widget() {
       <BottomSheet
         ref={bottomSheetRef}
         snapPoints={[1,200]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
       >
+        <Options />
         
       </BottomSheet>
     </>
